e133d8bbbc3e973891f827ec858496ca9294f842
LucianoFO11 <lucianosaulo05@gmail.com>
2025-08-14 21:34:06 -0300

    Atividade 3 funções e API's

---
+++ Def&Api-3.py
@@ -0,0 +1,20 @@
+import requests
+
+def pegarinfopoke(nomepokemon):
+    url = f"https://pokeapi.co/api/v2/pokemon/{nomepokemon.lower()}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Pokémon não encontrado!")
+        return
+
+    dados = response.json()
+
+    print("Nome:", dados.get("name"))
+    print("Peso:", dados.get("weight") / 10, "kg")  # peso vem em hectogramas
+    print("Altura:", dados.get("height") / 10, "m") # altura vem em decímetros
+    tipos = dados.get("types")
+    tipos_nomes = [t["type"]["name"] for t in tipos]
+    print("Tipos:", ", ".join(tipos_nomes))
+
+pegarinfopoke(input("Digite o nome do Pokémon: "))
